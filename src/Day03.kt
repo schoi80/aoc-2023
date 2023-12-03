b6803601511dcf2c0f14ef7dfad8fca58f096fdd
Sage Choi <sage.choi@gmail.com>
2023-12-03 01:13:56 -0500

    day 3

---
+++ src/Day03.kt
@@ -0,0 +1,97 @@
+fun main() {
+
+    val input = readInput("Day03")
+
+    fun Char.isSymbol() = !isDigit() && this != '.'
+
+    fun isPartNumber(row: Int, col: Int): Boolean {
+        println("evaluating input[$row][$col]")
+        if (input[row][col].isDigit()) {
+            val rows = ((row - 1)..(row + 1)).filter {
+                it >= 0 && it < input.size
+            }
+            val cols = ((col - 1)..(col + 1)).filter {
+                it >= 0 && it < input[row].length
+            }
+            rows.forEach { r ->
+                cols.forEach { c ->
+                    if (input[r][c].isSymbol())
+                        return true
+                }
+            }
+        }
+        return false
+    }
+
+    fun findNumber(row: Int): List<Int> {
+        val line = input[row]
+        val regex = "\\d+".toRegex()
+        val result = regex.findAll(line).filter {
+            it.range.forEach { col ->
+                if (isPartNumber(row, col)) {
+                    println("${it.value} is part")
+                    return@filter true
+                }
+            }
+            return@filter false
+        }.map { it.value.toInt() }.toList()
+        return result
+    }
+
+    fun isPartNumber2(row: Int, col: Int): Pair<Int, Int>? {
+        println("evaluating input[$row][$col]")
+        if (input[row][col].isDigit()) {
+            val rows = ((row - 1)..(row + 1)).filter {
+                it >= 0 && it < input.size
+            }
+            val cols = ((col - 1)..(col + 1)).filter {
+                it >= 0 && it < input[row].length
+            }
+            rows.forEach { r ->
+                cols.forEach { c ->
+                    if (input[r][c].isSymbol())
+                        return r to c
+                }
+            }
+        }
+        return null
+    }
+
+    fun findNumber2(row: Int): List<Pair<Int, Pair<Int, Int>>> {
+        val line = input[row]
+        val regex = "\\d+".toRegex()
+        val result = regex.findAll(line).mapNotNull {
+            it.range.forEach { col ->
+                val symbolCoordinate = isPartNumber2(row, col)
+                if (symbolCoordinate != null) {
+                    return@mapNotNull it.value.toInt() to symbolCoordinate
+                }
+            }
+            null
+        }.toList()
+        return result
+    }
+
+    fun part1(input: List<String>): Int {
+        return input.indices.flatMap { row ->
+            findNumber(row)
+        }.sum()
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.indices.flatMap { row ->
+            findNumber2(row)
+        }.filter { (_, gear) ->
+            input[gear.first][gear.second] == '*'
+        }.groupBy {
+            it.second
+        }.filterValues {
+            it.size == 2
+        }.values.sumOf {
+            it.map { it.first }.reduce { acc, it -> acc * it }
+        }
+    }
+
+    part1(input).println()
+    part2(input).println()
+}
