cf2b2f7bfce05046e06806059ad69317890cd422
Sage Choi <sage.choi@gmail.com>
2023-12-03 01:50:43 -0500

    day 3

--- src/Day03.kt
+++ src/Day03.kt
@@ -10,7 +10,7 @@ fun main() {
     }
 
     fun RowCol.isSymbol(): Boolean {
-        if (!this.isOutOfBounds()) {
+        if (!isOutOfBounds()) {
             val c = input[first][second]
             return !c.isDigit() && c != '.'
         }
@@ -18,7 +18,6 @@ fun main() {
     }
 
     fun RowCol.isPartNumber(): Boolean {
-        println("evaluating input[$first][$second]")
         if (input[first][second].isDigit()) {
             ((first - 1)..(first + 1)).forEach { r ->
                 ((second - 1)..(second + 1)).forEach { c ->
@@ -33,16 +32,14 @@ fun main() {
     fun findNumber(row: Int): List<Int> {
         val line = input[row]
         val regex = "\\d+".toRegex()
-        val result = regex.findAll(line).filter {
+        return regex.findAll(line).filter {
             it.range.forEach { col ->
                 if ((row to col).isPartNumber()) {
-                    println("${it.value} is part")
                     return@filter true
                 }
             }
             return@filter false
         }.map { it.value.toInt() }.toList()
-        return result
     }
 
     fun RowCol.isPartNumber2(): RowCol? {
@@ -60,7 +57,7 @@ fun main() {
     fun findNumber2(row: Int): List<Pair<Int, RowCol>> {
         val line = input[row]
         val regex = "\\d+".toRegex()
-        return regex.findAll(line).mapNotNull {mr ->
+        return regex.findAll(line).mapNotNull { mr ->
             mr.range.forEach { col ->
                 (row to col).isPartNumber2()?.let {
                     return@mapNotNull mr.value.toInt() to it
@@ -71,23 +68,15 @@ fun main() {
     }
 
     fun part1(input: List<String>): Int {
-        return input.indices.flatMap { row ->
-            findNumber(row)
-        }.sum()
+        return input.indices.flatMap { findNumber(it) }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.indices.flatMap { row ->
-            findNumber2(row)
-        }.filter { (_, rc) ->
-            input[rc.first][rc.second] == '*'
-        }.groupBy {
-            it.second
-        }.filterValues {
-            it.size == 2
-        }.values.sumOf {
-            it.map { it.first }.reduce { acc, it -> acc * it }
-        }
+        return input.indices.flatMap { findNumber2(it) }
+            .filter { (_, rc) -> input[rc.first][rc.second] == '*' }
+            .groupBy { it.second }
+            .filterValues { it.size == 2 }
+            .values.sumOf { it.map { it.first }.reduce { acc, it -> acc * it } }
     }
 
     part1(input).println()
