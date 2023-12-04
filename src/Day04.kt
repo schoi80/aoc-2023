287110e0927c5ddf6530a6c20ba432f68d7e44c9
Sage Choi <sage.choi@gmail.com>
2023-12-04 00:46:53 -0500

    day 4

--- src/Day04.kt
+++ src/Day04.kt
@@ -5,12 +5,20 @@ fun main() {
     val input = readInput("Day04")
 
     fun winningNumbers(line: String): Set<String> {
-        return line.split("|")[0].trim().split(":")[1].trim().split("\\s+".toRegex()).toSet()
+        return line.split("|")[0].trim()
+            .split(":")[1].trim()
+            .split("\\s+".toRegex()).toSet()
     }
 
-    fun score(line: String, winNums: Set<String>): Int {
-        val myNums = line.split("|")[1].trim().split(" ").toSet()
-        val count = myNums.filter { winNums.contains(it) }.toSet().count()
+    fun String.matchCount(winNums: Set<String>): Int {
+        return split("|")[1].trim()
+            .split(" ").toSet()
+            .filter { winNums.contains(it) }.toSet()
+            .count()
+    }
+
+    fun String.score(winNums: Set<String>): Int {
+        val count = matchCount(winNums)
         if (count == 1)
             return 1
         else if (count > 1)
@@ -18,30 +26,19 @@ fun main() {
         else return 0
     }
 
-    fun score2(line: String, winNums: Set<String>): Int {
-        val myNums = line.split("|")[1].trim().split(" ").toSet()
-        return myNums.filter { winNums.contains(it) }.toSet().count()
-    }
-
     fun part1(input: List<String>): Int {
-        return input.sumOf { line ->
-            winningNumbers(line).let { winningNumbers ->
-                score(line, winningNumbers).also { println("score: $it") }
-            }
-        }
+        return input.sumOf { line -> line.score(winningNumbers(line)) }
     }
 
     fun part2(input: List<String>): Int {
         val copies = mutableMapOf<Int, Int>()
         input.forEachIndexed { i, line ->
             copies[i] = copies[i] ?: 1
-            winningNumbers(line).let { winningNumbers ->
-                val count = score2(line, winningNumbers)
-                if (count > 0) {
-                    (1..count).forEach {
-                        val gameCard = it + i
-                        copies[gameCard] = copies[i]!! + (copies[i + it] ?: 1)
-                    }
+            val count = line.matchCount(winningNumbers(line))
+            if (count > 0) {
+                (1..count).forEach {
+                    val gameCard = it + i
+                    copies[gameCard] = copies[i]!! + (copies[i + it] ?: 1)
                 }
             }
         }
