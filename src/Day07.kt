d4451993a68064de885708ef4c0e106ae3fa4101
Sage Choi <sage.choi@gmail.com>
2023-12-07 01:17:57 -0500

    day 7 - code cleanup

--- src/Day07.kt
+++ src/Day07.kt
@@ -40,25 +40,16 @@ fun main() {
         }
     }
 
-    fun compare(c1: Char, c2: Char) = c1.score() - c2.score()
-
-
-    val scoreComparator = Comparator<Triple<Map<Char, Int>, Long, String>> { h1, h2 ->
+    val compareHands = Comparator<Triple<Map<Char, Int>, Long, String>> { h1, h2 ->
         if (h1.first.score() != h2.first.score())
             return@Comparator h1.first.score() - h2.first.score()
-
-        val p1 = h1.third
-        val p2 = h2.third
-        if (p1[0] != p2[0]) {
-            return@Comparator compare(p1[0], p2[0])
-        } else if (p1[1] != p2[1]) {
-            return@Comparator compare(p1[1], p2[1])
-        } else if (p1[2] != p2[2]) {
-            return@Comparator compare(p1[2], p2[2])
-        } else if (p1[3] != p2[3]) {
-            return@Comparator compare(p1[3], p2[3])
+        (0..4).forEach {
+            val p1 = h1.third[it]
+            val p2 = h2.third[it]
+            if (p1 != p2)
+                return@Comparator p1.score() - p2.score()
         }
-        return@Comparator return@Comparator compare(p1[4], p2[4])
+        error("this should never happen")
     }
 
 
@@ -90,35 +81,28 @@ fun main() {
             'A' -> 1
             'K' -> 2
             'Q' -> 3
-            'J' -> 15
+            'J' -> 15 // J gets super-power
             'T' -> 5
             else -> 10 - this.digitToInt() + 5
         }
     }
 
-    fun compare2(c1: Char, c2: Char) = c1.score2() - c2.score2()
-
-    val scoreComparator2 = Comparator<Triple<Map<Char, Int>, Long, String>> { h1, h2 ->
+    val compareHands2 = Comparator<Triple<Map<Char, Int>, Long, String>> { h1, h2 ->
         if (h1.first.score2() != h2.first.score2())
             return@Comparator h1.first.score2() - h2.first.score2()
 
-        val p1 = h1.third
-        val p2 = h2.third
-        if (p1[0] != p2[0]) {
-            return@Comparator compare2(p1[0], p2[0])
-        } else if (p1[1] != p2[1]) {
-            return@Comparator compare2(p1[1], p2[1])
-        } else if (p1[2] != p2[2]) {
-            return@Comparator compare2(p1[2], p2[2])
-        } else if (p1[3] != p2[3]) {
-            return@Comparator compare2(p1[3], p2[3])
+        (0..4).forEach {
+            val p1 = h1.third[it]
+            val p2 = h2.third[it]
+            if (p1 != p2)
+                return@Comparator p1.score2() - p2.score2()
         }
-        return@Comparator return@Comparator compare2(p1[4], p2[4])
+        error("this should never happen")
     }
 
     fun part1(input: List<String>): Long {
         return input.map { it.parse() }
-            .sortedWith(scoreComparator)
+            .sortedWith(compareHands)
             .reversed()
             .mapIndexed { i, v -> (i + 1) * v.second }
             .sum()
@@ -126,7 +110,7 @@ fun main() {
 
     fun part2(input: List<String>): Long {
         return input.map { it.parse() }
-            .sortedWith(scoreComparator2)
+            .sortedWith(compareHands2)
             .reversed()
             .mapIndexed { i, v -> (i + 1) * v.second }
             .sum()
