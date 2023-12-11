756036b89abb843d2fa50c274572ec8f937181df
Sage Choi <sage@wonder.com>
2023-12-10 19:18:54 -0500

    day 11 ready

---
+++ src/Day11.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    val input = readInput("sample")
+
+    fun part1(input: List<String>): Long {
+        return 0L
+    }
+
+    fun part2(input: List<String>): Long {
+        return 0L
+    }
+
+    part1(input).println()
+    part2(input).println()
+}
