72d021a9190cf2405920f154a1c8ffcfbbdd6650
Sage Choi <sage@wonder.com>
2023-12-14 03:13:36 -0500

    day 14. brute force is never the right solution

--- src/Day14.kt
+++ src/Day14.kt
@@ -3,7 +3,7 @@ fun MutableInput<Char>.tiltNorth(): MutableInput<Char> {
         var rockPosition = -1
         var rockCount = 0
         for (i in this.indices) {
-            when(this[i][j]) {
+            when (this[i][j]) {
                 '.' -> continue
                 'O' -> rockCount++
                 else -> {
@@ -40,7 +40,7 @@ fun MutableInput<Char>.tiltWest(): MutableInput<Char> {
         var rockPosition = -1
         var rockCount = 0
         for (j in this[i].indices) {
-            when(this[i][j]) {
+            when (this[i][j]) {
                 '.' -> continue
                 'O' -> rockCount++
                 else -> {
@@ -71,12 +71,13 @@ fun MutableInput<Char>.tiltWest(): MutableInput<Char> {
     }
     return this
 }
+
 fun MutableInput<Char>.tiltSouth(): MutableInput<Char> {
     for (j in this[0].indices) {
         var rockPosition = this.size
         var rockCount = 0
         for (i in this.size - 1 downTo 0) {
-            when(this[i][j]) {
+            when (this[i][j]) {
                 '.' -> continue
                 'O' -> rockCount++
                 else -> {
@@ -108,12 +109,12 @@ fun MutableInput<Char>.tiltSouth(): MutableInput<Char> {
     return this
 }
 
-fun MutableInput<Char>.tiltEast(): MutableInput<Char>  {
+fun MutableInput<Char>.tiltEast(): MutableInput<Char> {
     for (i in this.indices) {
         var rockPosition = this[i].size
         var rockCount = 0
         for (j in this[i].size - 1 downTo 0) {
-            when(this[i][j]) {
+            when (this[i][j]) {
                 '.' -> continue
                 'O' -> rockCount++
                 else -> {
@@ -147,12 +148,15 @@ fun MutableInput<Char>.tiltEast(): MutableInput<Char>  {
 
 fun MutableInput<Char>.calcLoad(): Long {
     return this.mapIndexed { index, s ->
-        s.count{it == 'O'} * (this.size - index.toLong())
+        s.count { it == 'O' } * (this.size - index.toLong())
     }.sum()
 }
 
-val cacheCycle = mutableMapOf<MutableInput<Char>, MutableInput<Char>>()
-fun MutableInput<Char>.cycle() = cacheCycle.getOrPut(this) { this.tiltNorth().tiltWest().tiltSouth().tiltEast() }
+fun MutableInput<Char>.getHash(): Int {
+    return this.joinToString("") { it.joinToString("") }.hashCode()
+}
+
+fun MutableInput<Char>.cycle() = this.tiltNorth().tiltWest().tiltSouth().tiltEast()
 
 fun main() {
 
@@ -163,11 +167,19 @@ fun main() {
 
     fun part2(input: List<String>): Long {
         var grid = input.toMutableInput { it }
-        var cycleCount = 1000000000
-        while(cycleCount>0) {
-            println(cycleCount)
+        val cycleCount = 1000000000
+        var curr = 0
+        val cache = mutableMapOf<Int, Int>()
+        while (cycleCount > curr) {
             grid = grid.cycle()
-            cycleCount--
+            curr++
+            val seenBefore = cache.getOrPut(grid.getHash()) {curr}
+            if (seenBefore != curr) {
+                println("i've seen this at $seenBefore! curr: $curr")
+                println("cycle at every ${curr - seenBefore}")
+                curr = cycleCount - ((cycleCount - curr) % (curr - seenBefore))
+                println("advancing to $curr")
+            }
         }
         return grid.calcLoad()
     }
